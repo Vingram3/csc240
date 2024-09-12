a72f87a252ba9493d10c5dcf40aa16cd6f6c14aa
Vingram3 <victoriaingram21@gmail.com>
2024-09-12 13:40:17 -0500

    First

---
+++ First/src/firstGH/Hello.java
@@ -0,0 +1,10 @@
+package firstGH;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ First/src/fistGH/Hello.java
@@ -0,0 +1,9 @@
+package fistGH;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	System.out.print("Hello");
+	}
+
+}
